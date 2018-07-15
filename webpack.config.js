5e3fa074254fae6d871577c8b04dd08c89e026c0
Leslie Zacharkow <lesliezacharkow@gmail.com>
2018-07-15 11:41:42 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    filename: 'site.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [ 'style-loader', 'css-loader' ]
+    }],
+  },
+};
