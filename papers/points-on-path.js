44f213cd3726982bb0899c4ddeafe1992823996e
Leslie Zacharkow <lesliezacharkow@gmail.com>
2018-07-16 05:39:52 -0700

    Sin wave path

--- papers/points-on-path.js
+++ papers/points-on-path.js
@@ -1,22 +1,30 @@
+import _ from 'lodash';
 import Pulp from './Pulp';
 
 class App extends Pulp {
 	draw() {
 		this.paths = [];
 
-		const offset = 100;
+		const offsetX = 200;
+		const offsetY = 100;
 
-		for (let i = offset; i < this.scene.view.bounds.right - offset; i += 7) {
+		for (let i = offsetX; i < this.scene.view.bounds.right - offsetX; i += 10) {
 			const scaleFactor = 0.0115398163;
 			const midPoint = this.scene.view.size.height / 2;
-			const sin = (this.scene.view.size.height - offset * 2) * 0.25;
-			const sinMidPoint = Math.sin((i - offset) * scaleFactor) * sin;
+			const sin = (this.scene.view.size.height - offsetY * 2) * 0.15;
+			const sinMidPoint = Math.sin((i - offsetY) * scaleFactor) * sin;
 
 			const path = new this.scene.Path(
-				new this.scene.Point(i, offset),
-				new this.scene.Point(i - 50, midPoint + sinMidPoint),
-				new this.scene.Point(i, this.scene.view.bounds.bottom - offset),
+				new this.scene.Point(i, offsetY),
+				new this.scene.Point(i, midPoint + sinMidPoint),
+				new this.scene.Point(i, this.scene.view.bounds.bottom - offsetY),
 			);
+
+			_.first(path.segments).handleOut = new this.scene.Point(-100, 100);
+			path.segments[1].handleIn = new this.scene.Point(-50, -50);
+
+			path.segments[1].handleOut = new this.scene.Point(-50, 50);
+			_.last(path.segments).handleIn = new this.scene.Point(-100, -100);
 
 			path.strokeColor = 'black';
 			this.paths.push(path);
